3de89af37b23b2124e63e48885f1024c0cbb67e9
allan-solvios <allan@solvios.technology>
2024-03-22 11:48:52 +0530

    Add project files.

---
+++ TestConsole1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Cannolai.Hubspot;
+using Cannolai.Hubspot.Entity;
+using TestConsole1;
+
+Console.WriteLine("Adding column started.");
+
+
+HubspotUtility hubspotUtility = new HubspotUtility();
+
+var result = await hubspotUtility.AddContactColumnAsync(TestData.CustomerIDColumn, new List<string> { "pat-na1-be2e5c9a-c29b-4113-8e5b-e523b19a8209" });
+
+Console.WriteLine("Process completed.");
+
+Console.ReadLine();
+
---
+++ TestConsole1/TestData.cs
@@ -0,0 +1,27 @@
+﻿using Cannolai.Hubspot.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole1
+{
+    public static class TestData
+    {
+        public static ContactColumn CustomerIDColumn { get; } = new ContactColumn
+        {
+            name = "x_CustomerID",
+            label = "x_Customer ID",
+            description = null,
+            type = "number",
+            fieldType = "number",
+            formField = true,
+            displayOrder = -1,
+            groupName = "cannolai",
+            hidden = false,
+            isRequired = true
+        };
+    }
+    
+}
